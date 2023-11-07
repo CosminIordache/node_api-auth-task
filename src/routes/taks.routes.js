4f364bb64fc100165087a085cea48b33c4da9f91
CosminIordache <cosminionutiordache@gmail.com>
2023-11-07 11:35:53 +0100

    validation task

--- src/routes/taks.routes.js
+++ src/routes/taks.routes.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { auth_requied } from '../middlewares/validate_token.js'
 import { getTasks, getTask, deleteTask, craeteTask, updatetTask } from '../controllers/task.controller.js';
+import { validateSchema } from '../middlewares/validator.middelware.js';
+import { createTaskSchema } from '../schemas/task.schema.js';
 
 const router = Router()
 
 router.get('/tasks', auth_requied , getTasks );
 router.get('/tasks/:id', auth_requied , getTask);
-router.post('/tasks', auth_requied , craeteTask);
+router.post('/tasks', auth_requied, validateSchema(createTaskSchema), craeteTask);
 router.delete('/tasks/:id', auth_requied , deleteTask);
 router.put('/tasks/:id', auth_requied , updatetTask);
 
