e3a3e95b923fbdd7bef5b23e1492a8a9f6353a13
CosminIordache <cosminionutiordache@gmail.com>
2023-11-10 13:52:19 +0100

    db change

--- src/db.js
+++ src/db.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 //MongoDB connection
 export const connectDB = async () => {
     try {
-        //mongoose.connect('mongodb+srv://node:node@cluster0.myqdxm4.mongodb.net/db?retryWrites=true&w=majority');
-        await mongoose.connect('mongodb://79.143.93.103:27017/users')
+        await mongoose.connect('mongodb')
         console.log('>>> DB connection done');
     } catch (error) {
         console.error('Mongoose connection error:', error);
