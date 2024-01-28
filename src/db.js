34fddb5e7e71100246ba62566778acb33161283e
CosminIordache <cosminionutiordache@gmail.com>
2024-01-28 23:40:04 +0100

    chando docker compose

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 //MongoDB connection
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1')
+        await mongoose.connect('mongodb://127.0.0.1:27018/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1')
         console.log('>>> DB connection done');
     } catch (error) {
         console.error('Mongoose connection error:', error);
